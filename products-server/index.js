a2fce718d2a8ce929967ed4179a9d59d8b3131bb
Sydney Beal <sydneybeal@miami.edu>
2018-12-19 00:10:18 -0500

    Code updated for AWS deployment The two apps are now launched on S3 (React) and ElasticBeanstalk (Node) Can successfully communicate to AWS MySQL Database

--- products-server/index.js
+++ products-server/index.js
@@ -1,23 +1,65 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const aws = require('aws-sdk');
+const ejs = require('ejs');
+var bodyParser = require('body-parser');
 
 const app = express();
 
-const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
+var port = process.env.PORT || 8081;
+
 
 const connection = mysql.createConnection({
+  /* // Local MySQL Setup
   host: 'localhost',
   user: 'root',
   password: 'nbuser',
   database: 'react_sql'
+  */
+  // AWS Setup
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DB_NAME
 });
 
+
+
 connection.connect(err => {
+  /*
   if(err) {
     return err;
   }
+  */
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+
 });
+
+
+
+// create the necessary table if necessary using SQL
+const createProductTable = `CREATE TABLE IF NOT EXISTS products(
+                        product_id int(11) not null auto_increment,
+                        name varchar(45) default null,
+                        price int(11) default null,
+                        PRIMARY KEY(product_id)
+                    )`;
+
+connection.query(createProductTable, (err, results) => {
+  if (err) {
+    console.log(err.message);
+  }
+});
+
+// set all products query
+const SELECT_ALL_PRODUCTS_QUERY = `SELECT * FROM products`;
 
 app.use(cors());
 
@@ -66,6 +108,6 @@ app.get('/products/remove', (req, res) => {
   })
 });
 
-app.listen(4000, () => {
-  console.log(`Products server listening on port 4000`)
+app.listen(8081, () => {
+  console.log(`Products server listening on port 8081`)
 });
